9a7be20ecc46596813f5016b34696360e0b252a1
Noah Holm <32292420+noppaz@users.noreply.github.com>
2020-12-05 16:07:04 +0100

    refactor: change number gen to SecureRandom

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,7 +3,7 @@ package controllers
 import play.api._
 import play.api.mvc._
 import javax.inject._
-import scala.util.Random
+import java.security.SecureRandom
 import services.Words
 
 @Singleton
@@ -26,7 +26,9 @@ class HomeController @Inject() (cc: ControllerComponents, words: Words)
       val extraLength = sep.length * (wordCount - 1) + end.length + numbers
       val wordList = words.getWords(wordCount, extraLength, min, 1)
       val wordString = wordList.mkString(sep)
-      val numberSeq = (Random.between(0.1, 1.0) * math.pow(10, numbers)).toInt
+      val randomGen = SecureRandom.getInstance("SHA1PRNG", "SUN")
+      val numberSeq =
+        String.format("%.0f", (randomGen.nextDouble * math.pow(10, numbers)))
       val password = wordString.capitalize + end + numberSeq.toString
       Ok(views.html.pw(password.length, wordList.length, password))
     }
